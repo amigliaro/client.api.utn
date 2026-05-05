71ca18aafc6ba8ff64181caf0ed38ea0c4601719
Andres Migliaro <52217777+amigliaro@users.noreply.github.com>
2026-05-05 12:19:24 -0300

    Se implementaron los test de client

--- src/test/java/com/client/api/models/ClientTest.java
+++ src/test/java/com/client/api/models/ClientTest.java
@@ -2,17 +2,25 @@ package com.client.api.models;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClientTest {
 
     @Test
     void esClienteReciente() {
-        // generar cliente que cumpla la condición y chequear que de true
+        // generar cliente que cumpla la condición y chequear que de true}
+        Client cliente = new Client(5L, "Diego", "Diaz", "","","","", LocalDate.of(2026,5,4));
+        boolean isOk = cliente.esClienteReciente(3);
+        assertTrue(isOk);
     }
 
     @Test
     void noEsClienteReciente() {
         // generar cliente que no cumpla la condición y chequear que de false
+        Client cliente = new Client(5L, "Diego", "Diaz", "","","","", LocalDate.of(2026,5,1));
+        boolean isOk = cliente.esClienteReciente(3);
+        assertFalse(isOk);
     }
 }
