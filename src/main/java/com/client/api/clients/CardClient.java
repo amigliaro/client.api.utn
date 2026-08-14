8fb1b5015c2defb1ca9cc1a3de1a78516e9cfc2d
Andres Migliaro <52217777+amigliaro@users.noreply.github.com>
2026-08-14 19:15:02 -0300

    Update CardClient.java

--- src/main/java/com/client/api/clients/CardClient.java
+++ src/main/java/com/client/api/clients/CardClient.java
@@ -4,7 +4,7 @@ import com.client.api.dto.Card;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name="cards-app", url = "${card.api.url}")
+@FeignClient(name="card.api")
 public interface CardClient {
     @GetMapping("/tarjetas/cliente/{idCliente}")
     Card obtenerProductos(Long idCliente);
